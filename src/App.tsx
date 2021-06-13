a28f4f526153ac32f3ad24a3e6011a83fe903c6e
Nahom Bekele <nahom.fix@gmail.com>
2021-06-13 18:19:17 +0300

    🚧 wip: implement UI without logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Autocomplete } from "@material-ui/lab";
+import { Box, TextField } from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const arr1: string[] = ["Arr1 option 1", "Arr1 option 2", "Arr1 option 3"];
+    const arr2: string[] = ["Arr2 option 1", "Arr2 option 2", "Arr2 option 3"];
+    const arr3: string[] = ["Arr3 option 1", "Arr3 option 2", "Arr3 option 3"];
+
+    return (
+        <Box m={2}>
+            <Box width={500} maxWidth="100%">
+                <Box mb={4}>
+                    <Autocomplete
+                        id="input1"
+                        freeSolo
+                        options={arr1}
+                        getOptionLabel={(option) => option}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Input 1"
+                                variant="outlined"
+                            />
+                        )}
+                    />
+                </Box>
+                <Box mb={4}>
+                    <Autocomplete
+                        id="input2"
+                        freeSolo
+                        options={arr2}
+                        getOptionLabel={(option) => option}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Input 2"
+                                variant="outlined"
+                            />
+                        )}
+                    />
+                </Box>
+                <Box mb={4}>
+                    <Autocomplete
+                        id="input3"
+                        freeSolo
+                        options={arr3}
+                        getOptionLabel={(option) => option}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Input 3"
+                                variant="outlined"
+                            />
+                        )}
+                    />
+                </Box>
+            </Box>
+        </Box>
+    );
 }
 
 export default App;
